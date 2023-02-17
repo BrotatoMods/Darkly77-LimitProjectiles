c5f0c06e0bf53bc5e00d1c84df7fff882f0a2cc1
Chris Bloomfield <43499897+ithinkandicode@users.noreply.github.com>
2023-02-17 07:46:35 +0000

    Remove debug lines

--- root/mods-unpacked/Darkly77-LimitProjectiles/extensions/entities/units/enemies/attack_behaviors/shooting_attack_behavior.gd
+++ root/mods-unpacked/Darkly77-LimitProjectiles/extensions/entities/units/enemies/attack_behaviors/shooting_attack_behavior.gd
@@ -3,9 +3,10 @@ extends "res://entities/units/enemies/attack_behaviors/shooting_attack_behavior.
 
 func shoot()->void :
 	var projectiles_on_screen = _projectiles_node.get_children().size()
+	# print("PROJECTILES_COUNT=" + str(projectiles_on_screen))
 	if projectiles_on_screen > 25: # I rarely got over 30 in my tests
 		# Reset cooldown
 		_current_cd = get_cd()
-		print("Cooldown reset, too many projectiles! Counted: " + str(projectiles_on_screen))
+		# print("Cooldown reset, too many projectiles! Counted: " + str(projectiles_on_screen))
 	else:
 		.shoot()
